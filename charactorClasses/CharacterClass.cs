5ce4458132a91b4c7333c03e854527bf8df7a9e4
Panida Paethanom <143201084+Panida287@users.noreply.github.com>
2026-02-10 23:53:08 +0100

    added characterClass

--- charactorClasses/CharacterClass.cs
+++ charactorClasses/CharacterClass.cs
@@ -2,5 +2,40 @@ namespace csArbeidskrav1.charactorClasses;
 
 public class CharacterClass
 {
+    private string _name;
+    private int _hitDie;
+    private int _xpForLevel2;
+    private string _primeRequiste;
+
+    public string Name
+    {
+        get => _name;
+        set => _name = value;
+    }
+
+    public int HitDie
+    {
+        get => _hitDie;
+        set => _hitDie = value;
+    }
     
+    public int XpForLevel2
+    {
+        get => _xpForLevel2;
+        set => _xpForLevel2 = value;
+    }   
+    
+    public string PrimeRequiste
+    {
+        get => _primeRequiste;
+        set => _primeRequiste = value;
+    }
+
+    public CharacterClass(String name, int hitDie, int xpForLevel2, string primeRequiste)
+    {
+        _name = name;
+        _hitDie = hitDie;
+        _xpForLevel2 = xpForLevel2;
+        _primeRequiste = primeRequiste;
+    }
 }
